876f95897cfd096db324c978d00f7299dbcde38d
james.s.e.maranga <james.s.e.maranga@accenture.com>
2026-03-11 20:24:39 +0800

    Add First Configs for DB

--- hr/src/main/java/com/company/hr/HrApplication.java
+++ hr/src/main/java/com/company/hr/HrApplication.java
@@ -1,4 +1,11 @@
 package com.company.hr;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HrApplication {
+    public static void main(String[] args){
+        SpringApplication.run(HrApplication.class,args);
+    }
 }
