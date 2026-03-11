b94ce73a98c97f2eb126c67d0ed09a6cbc181879
james.s.e.maranga <james.s.e.maranga@accenture.com>
2026-03-11 21:09:18 +0800

    Add First Configs for DB

--- hr/src/main/java/com/company/hr/employee/entity/Employee.java
+++ hr/src/main/java/com/company/hr/employee/entity/Employee.java
@@ -1,11 +1,15 @@
 package com.company.hr.employee.entity;
 
+import com.company.hr.attendance.entity.AttendanceLog;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,8 +20,13 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(mappedBy ="employee")
+    private List<AttendanceLog> attendanceLogs;
+
     private String employeeCode;
     private String firstName;
     private String lastName;
     private String email;
+    private String department;
+    private String status;
 }
---
+++ hr/src/main/java/com/company/hr/employee/entity/EmployeeSalary.java
@@ -0,0 +1,28 @@
+package com.company.hr.employee.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class EmployeeSalary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    private double baseSalary;
+    private double overTimeRate;
+    private double taxRate;
+}
