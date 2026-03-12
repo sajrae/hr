b18e36b11a8220b223a2402710dc9d937d0991d1
james.s.e.maranga <james.s.e.maranga@accenture.com>
2026-03-12 21:55:38 +0800

    Add simple Get Mapping

--- hr/src/main/java/com/company/hr/employee/controller/EmployeeController.java
+++ hr/src/main/java/com/company/hr/employee/controller/EmployeeController.java
@@ -1,13 +1,41 @@
 package com.company.hr.employee.controller;
 
+import com.company.hr.employee.entity.Department;
+import com.company.hr.employee.entity.Employee;
+import com.company.hr.employee.entity.EmployeeSalary;
+import com.company.hr.employee.entity.Position;
+import com.company.hr.employee.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping("/getEmployee")
-    public String getEmployee(){
-        return "Admin";
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employee")
+    public List<Employee> getEmployee(){
+        return employeeService.getEmployee();
+    }
+
+    @GetMapping("/salary")
+    public List<EmployeeSalary> getAllSalary(){
+        return employeeService.getSalaray();
+    }
+
+    @GetMapping("/department")
+    public List<Department> getAllDepartment(){
+        return employeeService.getAllDepartment();
+    }
+
+    @GetMapping("/position")
+    public List<Position> getAllPosition(){
+        return employeeService.getPosition();
     }
 }
--- hr/src/main/java/com/company/hr/employee/service/EmployeeService.java
+++ hr/src/main/java/com/company/hr/employee/service/EmployeeService.java
@@ -1,7 +1,45 @@
 package com.company.hr.employee.service;
 
+import com.company.hr.employee.entity.Department;
+import com.company.hr.employee.entity.Employee;
+import com.company.hr.employee.entity.EmployeeSalary;
+import com.company.hr.employee.entity.Position;
+import com.company.hr.employee.repository.DepartmentRepository;
+import com.company.hr.employee.repository.EmployeeRepository;
+import com.company.hr.employee.repository.EmployeeSalaryRepository;
+import com.company.hr.employee.repository.PositionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeSalaryRepository employeeSalaryRepository;
+    private final PositionRepository positionRepository;
+    private final DepartmentRepository departmentRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository, EmployeeSalaryRepository employeeSalaryRepository, PositionRepository positionRepository, DepartmentRepository departmentRepository) {
+        this.employeeRepository = employeeRepository;
+        this.employeeSalaryRepository = employeeSalaryRepository;
+        this.positionRepository = positionRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
+    public List<Employee> getEmployee(){
+        return employeeRepository.findAll();
+    }
+
+    public List<EmployeeSalary> getSalaray(){
+        return employeeSalaryRepository.findAll();
+    }
+
+    public List<Position> getPosition(){
+        return positionRepository.findAll();
+    }
+
+    public List<Department> getAllDepartment(){
+        return departmentRepository.findAll();
+    }
 }
