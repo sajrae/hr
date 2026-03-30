196df2217b82956440de2c3af901a15cea654769
sajrae <sajdev00@gmail.com>
2026-03-30 15:54:20 +0800

    fix CORS config issue

--- hr/src/main/java/com/company/hr/config/SecurityConfig.java
+++ hr/src/main/java/com/company/hr/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.company.hr.config;
 
 import com.company.hr.backoffice.view.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +13,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 
 @Configuration
@@ -42,5 +49,34 @@ public class SecurityConfig extends VaadinWebSecurity {
                 .build();
 
         return new InMemoryUserDetailsManager(admin);
+    }
+
+
+    /*
+     * Open API Config
+     * */
+    @Value("${app.server.url}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url(serverUrl)));
+    }
+
+    /*
+     * Cors Config
+     * */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // allow all (for now)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
